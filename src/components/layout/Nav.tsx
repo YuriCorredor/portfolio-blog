2d5e83854229be246f4defee595d97b50b4369a1
YuriCorredorFocus <yuri.corredor@focusnfe.com.br>
2023-04-03 23:05:23 -0300

    feat: :sparkles: add Nav

---
+++ src/components/layout/Nav.tsx
@@ -0,0 +1,94 @@
+import { signIn, signOut, useSession } from 'next-auth/react'
+import { forwardRef, Ref, useEffect, useRef, useState } from 'react'
+
+const DEFAULT_PROFILE_IMAGE = 'default-profile-pic.jpg'
+
+const Menu = forwardRef(({ showMenu }: { showMenu: boolean }, ref: Ref<HTMLDivElement>) => {
+  return (
+    <div
+      ref={ref}
+      className={`${showMenu ? 'opacity-100 scale-100' : 'opacity-0 scale-95 pointer-events-none'} absolute right-2 top-14 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none transition-all duration-75`}
+    >
+      <div className='py-1'>
+          <button
+            onClick={() => signOut()}
+            className='text-gray-700 block w-full px-4 py-2 text-left text-sm hover:text-gray-900 hover:bg-gray-200 transition-all'
+            >Sign out
+          </button>
+      </div>
+    </div>
+  )
+})
+
+export default function Nav() {
+  const menuRef = useRef<HTMLDivElement>(null)
+  const contentRef = useRef<HTMLDivElement>(null)
+  const [showMenu, setShowMenu] = useState(false)
+  const { data: sessionData } = useSession()
+  const isSignedIn = Boolean(sessionData)
+  const userName = sessionData?.user?.name
+  const userImage = sessionData?.user?.image || DEFAULT_PROFILE_IMAGE
+
+  const handleToogleMenu = () => {
+    setShowMenu(prevState => !prevState)
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        if (contentRef.current && !contentRef.current.contains(event.target as Node)) {
+          setShowMenu(false)
+        }
+      }
+    }
+
+    document.addEventListener('mousedown', handleClickOutside)
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, [menuRef, contentRef])
+
+  return (
+    <nav className='flex fixed w-full text-white transition-all'>
+      <div className='w-full justify-center flex p-4 border-b-[1px] bg-black bg-opacity-75 h-[69px] transition-all'>
+        <div className='max-w-7xl flex w-full justify-between items-center flex-row whitespace-nowrap'>
+          <div className='flex font-nav text-xl md:text-2xl lg:text-3xl font-extrabold'>
+            <a href='https://yuricorredor.com.br/' className='cursor-pointer hover:text-[#ddd] hover:scale-110 transition-all'>
+              <span>{`<`} Yuri Corredor</span>
+              <span className='pl-2'>{'/>'}</span>
+            </a>
+          </div>
+            {isSignedIn ? (
+              <>
+                <div
+                  ref={contentRef}
+                  onClick={handleToogleMenu} className='flex items-center cursor-pointer'
+                >
+                  <img
+                    className='rounded-full h-10 w-10 object-cover'
+                    src={userImage}
+                    alt='Profile Image'
+                  />
+                  <span className='ml-2 hidden sm:block text-sm'>{userName}</span>
+                </div>
+                <Menu
+                  showMenu={showMenu}
+                  ref={menuRef}
+                />
+              </>
+            ) : (
+              <div className='flex items-center'>
+                <button
+                  className='px-4 py-2 font-semibold text-sm bg-blue-500 text-white rounded-md shadow-sm hover:bg-indigo-500 ease-in-out duration-150'
+                  onClick={() => signIn()}
+                >
+                  Log in
+                </button>
+              </div>
+            )}
+          </div>
+      </div>
+    </nav>
+  )
+}
