93f9fcc9caaec9be89215815bd739200df90396d
Yuri Corredor <yuri.corredor@focusnfe.com.br>
2024-04-10 19:53:18 -0300

    Refactor Comment.tsx to improve code readability and handle null author values

--- src/components/posts/Comment.tsx
+++ src/components/posts/Comment.tsx
@@ -11,7 +11,7 @@ type Props = {
       image: string | null;
       name: string | null;
       id: string;
-    };
+    } | null;
   }
 }
 
@@ -28,7 +28,7 @@ export function Comment({ comment }: Props) {
     new Date(comment.createdAt),
     new Date(),
   ).replace('about ', '')
-  const isOwner = sessionData?.user?.id === author.id
+  const isOwner = author?.id ? sessionData?.user?.id === author?.id : false
 
   const handleDeleteComment = async () => {
     await mutateAsync({
@@ -43,14 +43,14 @@ export function Comment({ comment }: Props) {
   >
     <img
       className='w-10 h-10 rounded-full'
-      src={author.image || DEFAULT_PROFILE_IMAGE}
+      src={author?.image || DEFAULT_PROFILE_IMAGE}
       alt='User profile image'
     />
     <div className='w-full flex flex-col mx-4 break-words overflow-hidden'>
       <div className='flex items-center justify-between w-full mb-1'>
         <div className='flex items-center'>
           <p className='text-sm font-semibold text-gray-200 max-w-xs whitespace-nowrap overflow-hidden text-ellipsis'>
-            {author.name}
+            {author?.name  ? author.name : 'Anonymous'}
           </p>
           <p className='ml-3 text-xs text-gray-400 whitespace-nowrap'>
             {formattedDateFrom} ago
